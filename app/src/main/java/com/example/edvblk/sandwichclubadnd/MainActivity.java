0f6a6d453a25667f7d54b2297d03f4923b51a930
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 16:53:15 +0200

    Apply starter code

--- app/src/main/java/com/example/edvblk/sandwichclubadnd/MainActivity.java
+++ app/src/main/java/com/example/edvblk/sandwichclubadnd/MainActivity.java
@@ -1,4 +1,30 @@
 package com.example.edvblk.sandwichclubadnd;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, sandwiches);
+
+        ListView listView = findViewById(R.id.sandwiches_listview);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                DetailActivity.start(getApplicationContext(), position);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/edvblk/sandwichclubadnd/utils/JsonUtils.java
@@ -0,0 +1,12 @@
+package com.example.edvblk.sandwichclubadnd.utils;
+
+import com.example.edvblk.sandwichclubadnd.Sandwich;
+
+public final class JsonUtils {
+    private JsonUtils(){
+        // empty
+    }
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
