7b53bd783a758c9bc79e2f2a630fc598e5aed0e7
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 18:04:23 +0200

    Reformat code in MainActivity

--- app/src/main/java/com/example/edvblk/sandwichclubadnd/MainActivity.java
+++ app/src/main/java/com/example/edvblk/sandwichclubadnd/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.edvblk.sandwichclubadnd;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
