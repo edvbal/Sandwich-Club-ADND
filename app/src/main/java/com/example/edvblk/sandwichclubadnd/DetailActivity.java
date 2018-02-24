bd9828dffd830f7fbfa146e77bccd08436872967
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 18:04:03 +0200

    Implement DetailActivity#populateUi

--- app/src/main/java/com/example/edvblk/sandwichclubadnd/DetailActivity.java
+++ app/src/main/java/com/example/edvblk/sandwichclubadnd/DetailActivity.java
@@ -3,16 +3,35 @@ package com.example.edvblk.sandwichclubadnd;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.edvblk.sandwichclubadnd.utils.JsonUtils;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class DetailActivity extends AppCompatActivity {
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.textViewOtherNames)
+    TextView textViewOtherNames;
+    @BindView(R.id.textViewPlace)
+    TextView textViewPlace;
+    @BindView(R.id.textViewDescription)
+    TextView textViewDescription;
+    @BindView(R.id.textViewIngredients)
+    TextView textViewIngredients;
     public static final String EXTRA_POSITION = "extra_position";
-    private static final int DEFAULT_POSITION = -1;
+    private static final int POSITION_FAILURE_FLAG = -1;
+    private Unbinder unbinder;
 
     public static void start(Context context, int extra) {
         Intent starter = new Intent(context, DetailActivity.class);
@@ -24,44 +43,71 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
-        Intent intent = getIntent();
-        if (intent == null) {
-            closeOnError();
-        }
-
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
+        unbinder = ButterKnife.bind(this);
+        closeIfNoIntent();
+        int position = getIntent().getIntExtra(EXTRA_POSITION, POSITION_FAILURE_FLAG);
+        if (position == POSITION_FAILURE_FLAG) {
             closeOnError();
             return;
         }
-
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        String json = getSandwiches()[position];
+        Sandwich sandwich = JsonUtils.getSandwichFromJson(json);
         if (sandwich == null) {
-            // Sandwich data unavailable
             closeOnError();
             return;
         }
+        populateUI(sandwich);
+    }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+    @NonNull
+    private String[] getSandwiches() {
+        return getResources().getStringArray(R.array.sandwich_details);
+    }
 
-        setTitle(sandwich.getMainName());
+    private void closeIfNoIntent() {
+        if (getIntent() == null) {
+            closeOnError();
+        }
     }
 
     private void closeOnError() {
-        finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+        finish();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(imageView);
+        setAdditionalNames(sandwich);
+        textViewPlace.setText(sandwich.getPlaceOfOrigin());
+        textViewDescription.setText(sandwich.getDescription());
+        setIngredients(sandwich);
+    }
 
+    private void setAdditionalNames(Sandwich sandwich) {
+        List<String> names = sandwich.getAlsoKnownAs();
+        for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+            textViewOtherNames.append(String.format("%s ", names.get(i)));
+            if (i == names.size()) {
+                textViewOtherNames.append(names.get(i));
+            }
+        }
+    }
+
+    private void setIngredients(Sandwich sandwich) {
+        List<String> ingredients = sandwich.getIngredients();
+        for (int i = 0; i < ingredients.size(); i++) {
+            textViewIngredients.append(String.format("%s ", ingredients.get(i)));
+            if (i == ingredients.size()) {
+                textViewIngredients.append(ingredients.get(i));
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
     }
 }
